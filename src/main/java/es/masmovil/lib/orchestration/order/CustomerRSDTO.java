5e205d11056ab9af35ae016af9e38a94432ce51c
jmp87 <jaumin6@hotmail.com>
2020-12-07 12:44:14 +0100

    Init commit

---
+++ src/main/java/es/masmovil/lib/orchestration/order/CustomerRSDTO.java
@@ -0,0 +1,15 @@
+package es.masmovil.lib.orchestration.order;
+
+import lombok.Data;
+
+@Data
+public class CustomerRSDTO {
+	
+	private String identifier;
+	
+	private String name;
+	
+	private String surname;
+	
+	private String email;
+}
